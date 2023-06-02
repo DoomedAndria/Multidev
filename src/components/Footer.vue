0a16bfbaa22e7ef3a612d02347bf82a237d87b13
DoomedAndria <103760839+DoomedAndria@users.noreply.github.com>
2023-06-03 02:10:20 +0400

    small update in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -25,31 +25,48 @@ const links = [
 </script>
 
 <template>
-    <div class="w-full px-5 text-white flex flex-col bg-[#0B0B0B]">
+    <div class="w-full text-white flex justify-center bg-[#0B0B0B]">
+        <div class="w-full md:w-7/12  flex flex-col">
 
-        <div class="w-full flex flex-col">
-            <div>
-                <RouterLink to="/" class="my-12 inline-block">
-                    <MultiDevIcon />
-                </RouterLink>
-            </div>
-            <p class="text-sm leading-6 w-80 mb-2">
-                We help businesses generate more revenue
-                through strategic web design and SEO
-                services.
-            </p>
-            <h5 class="my-6">Follow Us</h5>
-            <div class="flex gap-6">
-                <FacebookIcon/>
-                <InstagramIcon/>
-                <TwitterIcon/>
-            </div>
-            <div class="w-full flex justify-end flex-wrap mt-8">
-                <div v-for="link in links" class="w-1/2 mb-4 pr-4  flex items-center text-sm">
-                    <RouterLink to="#">{{ link }}</RouterLink>
+            <div class="w-full px-5 flex flex-col pb-6">
+                <div>
+                    <RouterLink to="/" class="my-12 inline-block">
+                        <MultiDevIcon/>
+                    </RouterLink>
+                </div>
+                <div class="text-sm leading-6 max-w-xs flex mb-2">
+                    We help businesses generate more revenue
+                    through strategic web design and SEO
+                    services.
+                </div>
+                <h5 class="my-6">Follow Us</h5>
+                <div class="flex gap-6">
+                    <FacebookIcon/>
+                    <InstagramIcon/>
+                    <TwitterIcon/>
+                </div>
+                <div class="w-full flex justify-end flex-wrap mt-8">
+                    <div v-for="link in links" class="w-1/2 mb-4 pr-4  flex items-center text-sm">
+                        <RouterLink to="#">{{ link }}</RouterLink>
+                    </div>
                 </div>
             </div>
-        </div>
 
+            <hr class="opacity-30 border-[#D9D9D9]">
+
+            <div class="w-full px-5 py-10 flex flex-wrap text-[#EEF8FF] text-opacity-80">
+                <div class="w-1/2 mb-7 pr-4  flex items-center text-sm">
+                    Privacy Policy
+                </div>
+                <div class="w-1/2 mb-7 pr-4  flex items-center text-sm">
+                    Sitemap
+                </div>
+
+                <div class="w-full flex items-center text-sm">
+                    © 2023 - Icepick Development, LLC
+                </div>
+            </div>
+
+        </div>
     </div>
 </template>
