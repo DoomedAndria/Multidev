bc3384d14284cdbd80da5644f9d40cffbace73ee
DoomedAndria <103760839+DoomedAndria@users.noreply.github.com>
2023-06-24 00:46:44 +0400

    blog page finished

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+import MultiButton from "@/components/MultiButton.vue";
+
+const props = defineProps({
+    data: Object
+})
+</script>
+
+<template>
+    <div class="max-w-xl">
+        <div class="h-48 md:h-80 mb-5">
+            <img alt="123" class="w-full h-full object-cover" :src="data.image">
+        </div>
+        <h3 class="font-normal text-xl md:text-2xl mb-4 text-[#6967CE]">
+           {{data.title}}
+        </h3>
+        <p class="text-base text-[#407BEC] mb-4">
+            {{data.author}} | {{data.date}}
+        </p>
+        <p class="text-lg text-[#556D9A] mb-5">
+            {{data.content}}
+        </p>
+        <RouterLink to="/blog/123">
+            <MultiButton class="py-4 rounded-full">Read Post</MultiButton>
+        </RouterLink>
+
+    </div>
+</template>
--- src/views/pages/Blog.vue
+++ src/views/pages/Blog.vue
@@ -1,11 +1,59 @@
 <script setup>
 import BannerLayout from "@/layouts/BannerLayout.vue";
+import BlogCard from "@/components/BlogCard.vue";
+
+const posts = [
+    {
+        image: 'src/assets/images/Blog/p1.png',
+        title: 'Custom WordPress Development: A Comprehensive Guide',
+        author: 'Nick Meagher',
+        date: 'May 11, 2023',
+        content: 'Should you use a WordPress Theme or go with Custom WordPress\n' +
+            'Development from scratch? It all depends on your\n' +
+            'business goals.'
+    },
+    {
+        image: 'src/assets/images/Blog/p1.png',
+        title: 'Custom WordPress Development: A Comprehensive Guide',
+        author: 'Nick Meagher',
+        date: 'May 11, 2023',
+        content: 'Should you use a WordPress Theme or go with Custom WordPress\n' +
+            'Development from scratch? It all depends on your\n' +
+            'business goals.'
+    },
+    {
+        image: 'src/assets/images/Blog/p1.png',
+        title: 'Custom WordPress Development: A Comprehensive Guide',
+        author: 'Nick Meagher',
+        date: 'May 11, 2023',
+        content: 'Should you use a WordPress Theme or go with Custom WordPress\n' +
+            'Development from scratch? It all depends on your\n' +
+            'business goals.'
+    },
+    {
+        image: 'src/assets/images/Blog/p1.png',
+        title: 'Custom WordPress Development: A Comprehensive Guide',
+        author: 'Nick Meagher',
+        date: 'May 11, 2023',
+        content: 'Should you use a WordPress Theme or go with Custom WordPress\n' +
+            'Development from scratch? It all depends on your\n' +
+            'business goals.'
+    },
+]
 </script>
 
 <template>
     <BannerLayout>
-        <div class=' w-full flex justify-center items-center py-5'>
-            <h1 class='text-6xl shadow-slate-800'> Blog </h1>
+        <div class="px-5 py-10 md:py-24 bg-[#6967CE]">
+            <h1 class="font-bold text-4xl md:text-6xl text-white text-center">
+                Blog
+            </h1>
+        </div>
+        <div class="flex flex-col items-center px-5 py-10">
+            <div class="grid grid-cols-2 md:grid-cols-2 gap-5">
+                <BlogCard v-for="post in posts" :data="post"/>
+            </div>
+
         </div>
     </BannerLayout>
 </template>
